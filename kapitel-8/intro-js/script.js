bc117e351fa12ae28c45c7086b1118cfadcbc255
Sador Tilahun <sador.tilahun@elev.ga.ntig.se>
2025-03-13 14:26:25 +0100

    Tränat på Javascript, variabler, konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,16 @@
+// var tal1 = 12;
+// var tal2 = 22;
+// var summa = tal1 + tal2;
+// alert(tal1 + "+" + tal2 + "=" + summa);
+
+var förnamn = "Karim ";
+var efternamn = "Ryde";
+var ålder = 50;
+var namn = förnamn + efternamn;
+var meddelande = namn + " är " + ålder + " år gammal.";
+// alert(meddelande)
+
+var hobby = "goona";
+var kostand = 100;
+var meddelande = namn + " gillar att "+ hobby + "\noch det kostar " + kostand + " per år." ;  
+alert(meddelande)
